3174f2111c536c318116a0dd1e2101a520be948f
Awlter <wxfwalter@gmail.com>
2018-08-23 17:01:37 +0800

    forecast weather

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,10 @@
-//index.js
-//获取应用实例
 const weatherMap = {
   'sunny': '晴天',
   'cloudy': '多云',
   'overcast': '阴',
   'lightrain': '小雨',
   'heavyrain': '大雨',
-  'snow': '雪' 
+  'snow': '雪'
 }
 
 const weatherColorMap = {
@@ -21,37 +19,54 @@ const weatherColorMap = {
 Page({
   data: {
     nowTemp: '',
-    nowWeather: ''
+    nowWeather: '',
+    nowWeatherBackground: "",
+    hourlyWeather: []
   },
   onLoad() {
-    this.getNow(); 
+    this.getNow()
   },
   onPullDownRefresh() {
     this.getNow(() => {
       wx.stopPullDownRefresh()
-    });
+    })
   },
   getNow(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city: '上海'
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
+        city: '广州市'
       },
       success: res => {
         let result = res.data.result
-        let nowTemp = result.now.temp
-        let nowWeather = result.now.weather
+        let temp = result.now.temp
+        let weather = result.now.weather
         this.setData({
-          nowTemp: nowTemp + '°',
-          nowWeather: weatherMap[nowWeather],
-          nowWeatherBg: "/images/" + nowWeather + "-bg.png"
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png',
         })
         wx.setNavigationBarColor({
           frontColor: '#000000',
-          backgroundColor: weatherColorMap[nowWeather]
+          backgroundColor: weatherColorMap[weather],
+        })
+
+        let hourlyWeather = []
+        let nowHour = new Date().getHours()
+        let forecast = result.forecast
+
+        for(let i = 0; i < 24; i += 3) {
+          console.log(i)
+          hourlyWeather.push({
+            time: (i + nowHour) % 24 + '时',
+            iconPath: '/images/' + forecast[i/3].weather + '-icon.png',
+            temp: forecast[i / 3].temp + "°"
+          })
+        }
+
+        hourlyWeather[0].time = '现在'
+        this.setData({
+          hourlyWeather: hourlyWeather
         })
       },
       complete: () => {
